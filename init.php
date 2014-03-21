344700bf16396d6b76d7644e44cbfcfc5353a44d
itohsnap <itohsnap@users.noreply.github.com>
2014-03-21 00:41:31 -0700

    Bolt on a preference pane

--- init.php
+++ init.php
@@ -1,96 +1,201 @@
 <?php
 
-//
-// More info about this plugin in: https://github.com/atallo/ttrss_fullpost/
-//
-class Af_Full extends Plugin {
+// Initial version of this plugin: https://github.com/atallo/ttrss_fullpost/
+// Now with preference panel, ripped out of: https://github.com/mbirth/ttrss_plugin-af_feedmod
+// Relies on PHP-Readability: https://github.com/feelinglucky/php-readability
+
+// Expects the preference field to be valid JSON, which for arrays means we need square braces:
+// [
+//	 "kotaku.com",
+//	 "destructoid",
+//	 "arstechnica.com"
+// ]
+
+// Note that this will consider the feed to match if the feed's "link" URL contains any
+// element's text. Most notably, Destructoid's posts are linked through Feedburner, and
+// so "destructoid.com" doesn't match--but there is a "Destructoid" in the Feedburner URL,
+// so "destructoid" will. (Link comparisons are case-insensitive.)
+
+class Af_Fullpost extends Plugin implements IHandler
+{
 	private $host;
 
 	function about() {
-		return array(0.01,
-			"Full post (requires CURL). (Work in progress)",
+		return array(0.03,
+			"Full post (requires CURL).",
 			"atallo");
+	}
+	
+	function api_version() {
+		return 2;
 	}
 
 	function init($host) {
 		$this->host = $host;
 
+		$host->add_hook($host::HOOK_PREFS_TABS, $this);
 		$host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
 	}
-
+	
 	function hook_article_filter($article) {
 		if (!function_exists("curl_init"))
 			return $article;
-
-		if ((strpos($article["link"], "theinquirer.es") !== FALSE ||
-			strpos($article["link"], "silicon-news") !== FALSE ||
-			strpos($article["link"], "siliconnews") !== FALSE
-		)) {
-
-		    $article["content"] .= "<br/><hr/>Web original:<br><hr/>" .
-		        $this->get_full_post($article["link"]);
+		
+		$json_conf = $this->host->get($this, 'json_conf');
+		$owner_uid = $article['owner_uid'];
+		$data = json_decode($json_conf, true);
+		
+		if (!is_array($data)) {
+			// no valid JSON or no configuration at all
+			return $article;
 		}
-
+		
+		//foreach ($data as $urlpart=>$config) {
+		foreach ($data as $urlpart) {
+			// 2/23/14: stripos() is a case-insensitive version of strpos()
+			if (stripos($article['link'], $urlpart) === false) continue; // skip this entry, if the URL doesn't match
+			if (strpos($article['plugin_data'], "fullpost,$owner_uid:") !== false) {
+				// do not process an article more than once
+				if (isset($article['stored']['content'])) $article['content'] = $article['stored']['content'];
+				break;
+			}
+			
+			$article['content'] = $this->get_full_post($article['link']);
+			//$article["content"] .= "<br/><hr/>Web original:<br><hr/>" .
+			//	$this->get_full_post($article["link"]);
+			$article['plugin_data'] = "fullpost,$owner_uid:" . $article['plugin_data'];
+			break;
+		}
+		
 		return $article;
 	}
-
-	function api_version() {
-		return 2;
-	}
-
+	
 	private function get_full_post($request_url) {
-	    // https://github.com/feelinglucky/php-readability#
-
-	    include_once 'Readability.inc.php';
-
-	    //define("DIR_ROOT", dirname(__FILE__));
-	    //define("DIR_CACHE",  DIR_ROOT . '/cache');
-
-	    //$request_url = "http://www.siliconnews.es/2013/07/01/estos-son-los-sueldos-de-los-principales-directivos-de-la-industria-tecnologica/";
-
-	    //$request_url_hash = md5($request_url);
-	    //$request_url_cache_file = sprintf(DIR_CACHE."/%s.url", $request_url_hash);
-
-	    //if (file_exists($request_url_cache_file) &&
-	    //    (time() - filemtime($request_url_cache_file) < CACHE_TIME)) {
-            //
-	    //    $source = file_get_contents($request_url_cache_file);
-            //} else {
-
-                $handle = curl_init();
-                curl_setopt_array($handle, array(
-                    CURLOPT_USERAGENT => USER_AGENT,
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_HEADER  => false,
-                    CURLOPT_HTTPGET => true,
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_TIMEOUT => 30,
-                    CURLOPT_URL => $request_url
-                ));
-
-                $source = curl_exec($handle);
-                curl_close($handle);
-
-                // Write request data into cache file.
-            //    file_put_contents($request_url_cache_file, $source);
-            //}
-
-            //if (!$charset = mb_detect_encoding($source)) {
-            //}
-            preg_match("/charset=([\w|\-]+);?/", $source, $match);
-            $charset = isset($match[1]) ? $match[1] : 'utf-8';
-
-            $Readability = new Readability($source, $charset);
-            $Data = $Readability->getContent();
-
-            //$title   = $Data['title'];
-            //$content = $Data['content'];
-
-            //#include 'template/reader.html';
-            //echo $title;
-
-            return $Data['content'];
-        }
+		// https://github.com/feelinglucky/php-readability
+		
+		include_once 'Readability.inc.php';
+		
+		//define("DIR_ROOT", dirname(__FILE__));
+		//define("DIR_CACHE",  DIR_ROOT . '/cache');
+		
+		//$request_url = "http://www.siliconnews.es/2013/07/01/estos-son-los-sueldos-de-los-principales-directivos-de-la-industria-tecnologica/";
+		
+		//$request_url_hash = md5($request_url);
+		//$request_url_cache_file = sprintf(DIR_CACHE."/%s.url", $request_url_hash);
+		
+		//if (file_exists($request_url_cache_file) &&
+		//	(time() - filemtime($request_url_cache_file) < CACHE_TIME)) {
+		//		$source = file_get_contents($request_url_cache_file);
+		//	} else {
+		
+		$handle = curl_init();
+		curl_setopt_array($handle, array(
+			CURLOPT_USERAGENT => USER_AGENT,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HEADER  => false,
+			CURLOPT_HTTPGET => true,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_URL => $request_url
+		));
+		
+		$source = curl_exec($handle);
+		curl_close($handle);
+		
+		//		// Write request data into cache file.
+		//		file_put_contents($request_url_cache_file, $source);
+		//	}
+		
+		//if (!$charset = mb_detect_encoding($source)) {
+		//}
+		preg_match("/charset=([\w|\-]+);?/", $source, $match);
+		$charset = isset($match[1]) ? $match[1] : 'utf-8';
+		
+		$Readability = new Readability($source, $charset);
+		$Data = $Readability->getContent();
+		
+		//$title   = $Data['title'];
+		//$content = $Data['content'];
+		
+		//#include 'template/reader.html';
+		//echo $title;
+		
+		return $Data['content'];
+	}
+	
+	
+	function hook_prefs_tabs($args)
+	{
+		print '<div id="fullpostConfigTab" dojoType="dijit.layout.ContentPane"
+					href="backend.php?op=af_fullpost"
+					title="' . __('FullPost') . '"></div>';
+  }
+	
+	function index()
+	{
+		$pluginhost = PluginHost::getInstance();
+		$json_conf = $pluginhost->get($this, 'json_conf');
+		
+		print "<form dojoType=\"dijit.form.Form\">";
+		
+		print "<script type=\"dojo/method\" event=\"onSubmit\" args=\"evt\">
+			evt.preventDefault();
+			if (this.validate()) {
+				new Ajax.Request('backend.php', {
+					parameters: dojo.objectToQuery(this.getValues()),
+					onComplete: function(transport) {
+						if (transport.responseText.indexOf('error')>=0) notify_error(transport.responseText);
+						else notify_info(transport.responseText);
+					}
+				});
+				//this.reset();
+			}
+			</script>";
+		
+		print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"op\" value=\"pluginhandler\">";
+		print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"method\" value=\"save\">";
+		print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"plugin\" value=\"af_fullpost\">";
+		
+		print "<table width='100%'><tr><td>";
+		print "<textarea dojoType=\"dijit.form.SimpleTextarea\" name=\"json_conf\" style=\"font-size: 12px; width: 99%; height: 500px;\">$json_conf</textarea>";
+		print "</td></tr></table>";
+		
+		print "<p><button dojoType=\"dijit.form.Button\" type=\"submit\">".__("Save")."</button>";
+		
+		print "</form>";
+	}
+	
+	function save()
+	{
+		$json_conf = $_POST['json_conf'];
+		
+		if (is_null(json_decode($json_conf))) {
+			echo __("error: Invalid JSON!");
+			return false;
+		}
+		
+		$this->host->set($this, 'json_conf', $json_conf);
+		echo __("Configuration saved.");
+	}
+	
+	function csrf_ignore($method)
+	{
+		$csrf_ignored = array("index", "edit");
+		return array_search($method, $csrf_ignored) !== false;
+	}
+	
+	function before($method)
+	{
+		if ($_SESSION["uid"]) {
+			return true;
+		}
+		return false;
+	}
+	
+	function after()
+	{
+		return true;
+	}
 
 }
 ?>
